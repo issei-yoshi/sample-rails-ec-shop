854089e54db5aeb6b2d15d3f132b39ffe764789f
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-21 12:10:58 +0900

    add: seedsファイルに初期データを投入するコードを記載

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,51 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+item1 = Item.new(
+  name: "Cap",
+  price: 2000,
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+)
+item1.image.attach(io: File.open(Rails.root.join('app/assets/images/cap.jpg')), filename: 'cap.jpg')
+item1.save!
+
+item2 = Item.new(
+  name: "Glasses",
+  price: 5000,
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+)
+item2.image.attach(io: File.open(Rails.root.join('app/assets/images/glasses.jpg')), filename: 'glasses.jpg')
+item2.save!
+
+item3 = Item.new(
+  name: "Jacket",
+  price: 3000,
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+)
+item3.image.attach(io: File.open(Rails.root.join('app/assets/images/jacket.jpg')), filename: 'jacket.jpg')
+item3.save!
+
+item4 = Item.new(
+  name: "Pants",
+  price: 4000,
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+)
+item4.image.attach(io: File.open(Rails.root.join('app/assets/images/pants.jpg')), filename: 'pants.jpg')
+item4.save!
+
+item5 = Item.new(
+  name: "Shirt",
+  price: 1000,
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+)
+item5.image.attach(io: File.open(Rails.root.join('app/assets/images/shirt.jpg')), filename: 'shirt.jpg')
+item5.save!
+
+item6 = Item.new(
+  name: "Shoes",
+  price: 7000,
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+)
+item6.image.attach(io: File.open(Rails.root.join('app/assets/images/shoes.jpg')), filename: 'shoes.jpg')
+item6.save!
