43a94e9d549514d595c4b837d5db2bcfe80448ca
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-24 18:20:07 +0900

    add: routesにてcarts#showへのルーティングを定義してviewファイルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # resources :tasks
   root 'items#index'
   resources :items, only: %i[index show]
+  resources :carts, only: %i[show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
