03b9f44bda5cdaa913ff08a05c3ba4a5149e4914
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-24 18:36:10 +0900

    add: routesにてcarts#add_itemへのルーティングを定義してメソッドを定義

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,4 +2,7 @@ class CartsController < ApplicationController
   def show
     @carts = current_cart.cart_items
   end
+
+  def add_item
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root 'items#index'
   resources :items, only: %i[index show]
   resources :carts, only: %i[show]
+  post 'add_item' => 'carts#add_item'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
