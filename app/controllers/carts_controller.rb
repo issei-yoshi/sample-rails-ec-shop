e1538fce2ea36e3d0502bc9602ee4652504b9c4e
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-24 18:40:42 +0900

    add: cartsControllerにてadd_itemメソッドを定義

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,5 +4,14 @@ class CartsController < ApplicationController
   end
 
   def add_item
+    @cart_item = current_cart.cart_items.find_by(item_id: params[:item_id])
+
+    if @cart_item.blank?
+      @cart_item = current_cart.cart_items.build(item_id: params[:item_id])
+    end
+
+    @cart_item.quantity += params[:quantity].to_i
+    @cart_item.save
+    redirect_to current_cart
   end
 end
