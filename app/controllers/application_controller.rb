98e39720f4bb19de7670e2ebc552937824cb1136
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-24 18:17:10 +0900

    add: applicationControllerにcurrent_cartというhelperMethod(Cartモデルのインスタンスを返却するメソッド)を定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  helper_method :current_cart
+
+  def current_cart
+    if session[:cart_id]
+      @cart = Cart.find(session[:cart_id])
+    else
+      @cart = Cart.create()
+      session[:cart_id] = @cart.id
+      @cart
+    end
+  end
 end
