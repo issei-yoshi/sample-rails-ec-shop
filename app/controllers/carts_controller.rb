b7df66a3383ef742b52474d6c8a79826fc3ef524
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-25 10:19:21 +0900

    fix: carts_controllerのadd_itemメソッドにてfind_or_initialize_byを用いて、set_cart_itemコールバックを適用しないよう修正

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
 
 class CartsController < ApplicationController
-  before_action :set_cart_item, only: %i[add_item delete_item]
+  before_action :set_cart_item, only: %i[delete_item]
 
   def show
     @carts = current_cart.cart_items
   end
 
   def add_item
-    @cart_item = current_cart.cart_items.build(item_id: params[:item_id]) if @cart_item.blank?
-
+    @cart_item = current_cart.cart_items.find_or_initialize_by(item_id: params[:item_id])
     @cart_item.quantity += params[:quantity].to_i
     @cart_item.save
     redirect_to carts_path
