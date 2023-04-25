42e120211064f61a274904d75994c89005146ab6
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-25 19:09:20 +0900

    add: orderControllerのcreateアクションにて、OrderモデルのインスタンスとOrderDetailモデルのインスタンスを作成するよう実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,40 @@
 class OrdersController < ApplicationController
   def create
+    carts = current_cart.cart_items
+    price = current_cart.total_price
+
+    order = Order.new(order_params)
+    order.billing_amount = price
+
+    if order.save
+      carts.each do |item|
+        OrderDetail.create(
+          order_id: order.id,
+          item_id: item.item_id,
+          quantity: item.quantity,
+          price: item.item.price
+        )
+        item.destroy
+      end
+      redirect_to items_path
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(
+      :first_name,
+      :last_name,
+      :user_name,
+      :email,
+      :address,
+      :address_building,
+      :postal_code,
+      :cardholder_name,
+      :card_number,
+      :card_expiration,
+      :card_cvv
+      )
   end
 end
