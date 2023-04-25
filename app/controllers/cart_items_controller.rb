ba0163eec29974832afe89283d2f4f2c93fe372a
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-25 17:06:42 +0900

    fix: cart_itemsControllerのdestroyアクションを定義して各link_toのurlを変更

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CartItemsController < ApplicationController
   def create
     @cart_item = current_cart.cart_items.find_or_initialize_by(item_id: params[:item_id])
@@ -7,5 +9,8 @@ class CartItemsController < ApplicationController
   end
 
   def destroy
+    @cart_item = current_cart.cart_items.find_by!(item_id: params[:id])
+    @cart_item.destroy
+    redirect_to carts_path
   end
 end
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,10 +4,4 @@ class CartsController < ApplicationController
   def show
     @carts = current_cart.cart_items
   end
-
-  def destroy
-    @cart_item = current_cart.cart_items.find_by!(item_id: params[:item_id])
-    @cart_item.destroy
-    redirect_to carts_path
-  end
 end
