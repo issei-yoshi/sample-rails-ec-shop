57976d7780c3afa70e0b3bcbc83fe0dc62b57f3f
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-25 18:47:14 +0900

    add: Orderモデルにアソシエーションとvalidationを定義

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,17 @@
 class Order < ApplicationRecord
+  has_many :order_details
+
+  with_options presence: true do
+    validates :first_name
+    validates :last_name
+    validates :user_name
+    validates :email
+    validates :address
+    validates :cardholder_name
+    validates :card_number
+    validates :card_expiration
+    validates :card_cvv
+    validates :billing_amount
+    validates :postal_code
+  end
 end
