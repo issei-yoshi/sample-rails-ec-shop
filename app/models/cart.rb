c14834b56d4549c29d3b639a7edfa40cdf5755b3
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-25 10:41:14 +0900

    rubocopを通してauto correct

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,10 +4,10 @@ class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
 
   def total_price
-    cart_items.inject(0){|i, cart_item| i += (cart_item.item.price * cart_item.quantity)}
+    cart_items.inject(0) { |i, cart_item| i + (cart_item.item.price * cart_item.quantity) }
   end
 
   def total_quantity
-    cart_items.inject(0){|i, cart_item| i += cart_item.quantity}
+    cart_items.inject(0) { |i, cart_item| i + cart_item.quantity }
   end
 end
