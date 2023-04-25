09295dda3c7f3f48def95192af2bd670c1c0420a
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-25 16:54:33 +0900

    fix: cart_itemsControllerのcreateアクションを定義して各form_withのurlを変更

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,4 +1,8 @@
 class CartItemsController < ApplicationController
   def create
+    @cart_item = current_cart.cart_items.find_or_initialize_by(item_id: params[:item_id])
+    @cart_item.quantity += params[:quantity].to_i
+    @cart_item.save
+    redirect_to carts_path
   end
 end
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,13 +5,6 @@ class CartsController < ApplicationController
     @carts = current_cart.cart_items
   end
 
-  def create
-    @cart_item = current_cart.cart_items.find_or_initialize_by(item_id: params[:item_id])
-    @cart_item.quantity += params[:quantity].to_i
-    @cart_item.save
-    redirect_to carts_path
-  end
-
   def destroy
     @cart_item = current_cart.cart_items.find_by!(item_id: params[:item_id])
     @cart_item.destroy
