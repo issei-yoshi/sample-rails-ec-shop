e4f460caf5f32d371abee7655ff804bb47f873f2
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-25 10:24:43 +0900

    fix: routesのadd_itemをcreateアクションへと変更して、メソッド名もcreateメソッドへ修正

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -7,7 +7,7 @@ class CartsController < ApplicationController
     @carts = current_cart.cart_items
   end
 
-  def add_item
+  def create
     @cart_item = current_cart.cart_items.find_or_initialize_by(item_id: params[:item_id])
     @cart_item.quantity += params[:quantity].to_i
     @cart_item.save
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   # resources :tasks
   root 'items#index'
   resources :items, only: %i[index show]
-  resource :carts, only: %i[show]
-  post 'add_item' => 'carts#add_item'
+  resource :carts, only: %i[show create]
   delete '/delete_item' => 'carts#delete_item'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
