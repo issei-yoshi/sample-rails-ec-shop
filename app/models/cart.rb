c315512b259f4d91ffc451ae58f57228ee1cd2f4
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-25 09:58:50 +0900

    fix: cart.rbに定義しているtotal_priceメソッドをinjectを用いてリファクタリング

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,11 +4,7 @@ class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
 
   def total_price
-    total = 0
-    cart_items.each do |cart_item|
-      total += (cart_item.item.price * cart_item.quantity)
-    end
-    total
+    cart_items.inject(0){|i, cart_item| i += (cart_item.item.price * cart_item.quantity)}
   end
 
   def total_quantity
