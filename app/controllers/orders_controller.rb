551cc5e1fd7a65b34d429d50d4b0f614141c4abb
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-28 22:10:33 +0900

    fix: 金額の表示フォーマットを変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -28,7 +28,7 @@ class OrdersController < ApplicationController
 
   def basic
     authenticate_or_request_with_http_basic do |name, password|
-      name == ENV['BASIC_AUTH_NAME'] && password == ENV['BASIC_AUTH_PASSWORD']
+      name == 'admin' && password == 'pw'
     end
   end
 
