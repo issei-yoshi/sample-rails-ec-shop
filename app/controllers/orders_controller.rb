e6191e67967cc12bcecfaab6797be1a3da4573b7
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-28 22:28:40 +0900

    fix: basic認証をherokuの環境変数に変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -28,7 +28,7 @@ class OrdersController < ApplicationController
 
   def basic
     authenticate_or_request_with_http_basic do |name, password|
-      name == 'admin' && password == 'pw'
+      name == ENV['BASIC_AUTH_NAME'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 
