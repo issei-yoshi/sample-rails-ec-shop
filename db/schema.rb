29d59622827c6681add9537ea23859810e2d3fc2
issei-yoshi <iyoshi.engineer@gmail.com>
2023-05-02 13:23:37 +0900

    add: migrationファイルを実行してCartsテーブルにpromotion_codeカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_02_035947) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_02_042137) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_02_035947) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "promotion_code"
   end
 
   create_table "items", force: :cascade do |t|
