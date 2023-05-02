c731707406701400f367622ce213b2c5ab90966b
issei-yoshi <iyoshi.engineer@gmail.com>
2023-05-02 13:36:27 +0900

    add: carts/showのviewにてpromotion_codeの値を元に割引料金を表示するよう実装

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,5 +4,7 @@ class CartsController < ApplicationController
   def show
     @carts = current_cart.cart_items
     @order = Order.new
+    @code = current_cart.promotion_code
+    @discount = PromotionCode.find_by(code: @code)&.discount
   end
 end
