f934a7ee89e0bf9414bb9a5fcb6fc57fe16d38c3
issei-yoshi <iyoshi.engineer@gmail.com>
2023-05-02 13:28:42 +0900

    add: promotion_codesコントローラのcreateアクションを定義

--- app/controllers/promotion_codes_controller.rb
+++ app/controllers/promotion_codes_controller.rb
@@ -1,4 +1,17 @@
 class PromotionCodesController < ApplicationController
   def create
+    cart = current_cart
+    code = params[:promo_code]
+    promotion_code = PromotionCode.find_by(code: code)
+
+    if promotion_code && promotion_code.inactivated?
+      cart.update(
+        promotion_code: code
+      )
+      flash[:notice] = "プロモーションコードが適用されました"
+    else
+      flash[:notice] = "プロモーションコードが無効です"
+    end
+    redirect_to carts_path
   end
 end
