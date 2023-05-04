e4420e0ee2007b4da3edc951a44b0fa4ea9c912d
issei-yoshi <iyoshi.engineer@gmail.com>
2023-05-04 10:30:41 +0900

    add: migrationファイルを実行してOrdersテーブルにdiscountカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_02_042137) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_04_012616) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -91,6 +91,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_02_042137) do
     t.integer "billing_amount", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "discount"
   end
 
   create_table "promotion_codes", force: :cascade do |t|
