3b6c7586c4b23c714bfe7600944db6f129f35795
issei-yoshi <iyoshi.engineer@gmail.com>
2023-05-05 10:28:12 +0900

    add: cartsControllerのshowアクションにて@codeというインスタンス変数を削除

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,7 +4,6 @@ class CartsController < ApplicationController
   def show
     @carts = current_cart.cart_items
     @order = Order.new
-    @code = current_cart.promotion_code
-    @discount = PromotionCode.find_by(code: @code)&.discount || 0
+    @discount = PromotionCode.find_by(code: current_cart.promotion_code)&.discount || 0
   end
 end
